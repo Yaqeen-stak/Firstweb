c2d7b6c15ceed28cd9ef9c6ee62b217c7f6b4d72
Yaqeen hamd <engyaqeenhamd@yahoo.com>
2020-08-25 19:49:41 +0300

    javalink

---
+++ JS/Javaweb.js
@@ -0,0 +1,19 @@
+var Number = prompt('How many rose you will buy ??');
+var greeting;
+
+if ( Number < 100 && Number > 70) {
+greeting= 'YOU WILL GET 50% DISCOUNT';
+}
+
+else if (Number > 40 && Number < 70) {
+greeting = 'YOU WILL GET 30% DISCOUNT!';
+}
+
+else if (Number < 40 && Number > 20) {
+greeting = 'YOU WILL GIT 10% DISCOUNT! ' ;}
+else if (Number <20 ) {
+    greeting = 'NO DISCOUNT!!!';
+    }
+
+
+document .write( ' <h3>' +greeting + ' </ h3>');
