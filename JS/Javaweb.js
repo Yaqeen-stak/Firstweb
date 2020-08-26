a67b979331524f8e93b2633db8682c3aa5435abe
Yaqeen hamd <engyaqeenhamd@yahoo.com>
2020-08-26 13:06:27 +0300

    using the function

--- JS/Javaweb.js
+++ JS/Javaweb.js
@@ -1,7 +1,6 @@
 
 var Number = prompt('How many rose you will buy ??');
 var greeting;  
-
 if ( Number > 70) {
 greeting= 'YOU WILL GET 50% DISCOUNT';
 }
@@ -16,5 +15,24 @@ else if (Number <20 ) {
     greeting = 'NO DISCOUNT!!!';
     }
 
-
 document .write( ' <h3>' +greeting + ' </ h3>');
+
+function getGreeting (Number) {
+    console.log (Number);
+    if ( Number > 70) {
+        greeting= 'YOU WILL GET 50% DISCOUNT';
+        }
+        
+        else if (Number > 40 && Number < 70) {
+        greeting = 'YOU WILL GET 30% DISCOUNT!';
+        }
+        
+        else if (Number < 40 && Number > 20) {
+        greeting = 'YOU WILL GIT 10% DISCOUNT! ' ;}
+        else if (Number <20 ) {
+            greeting = 'NO DISCOUNT!!!';
+        }
+        document .write( ' <h3>' +greeting + ' </ h3>');
+}
+getGreeting (Number);     
+console.log ('Number of flowers required is :' + Number);
