04102ea30de3cae548818fece3c99da0490daf7b
Yaqeen hamd <engyaqeenhamd@yahoo.com>
2020-08-26 16:56:06 +0300

    wihle loop

--- JS/Javaweb.js
+++ JS/Javaweb.js
@@ -9,9 +9,9 @@ else if (Number > 40 && Number < 70) {
 greeting = 'YOU WILL GET 30% DISCOUNT!';
 }
 
-else if (Number < 40 && Number > 20) {
+else if (Number <= 40 && Number > 20) {
 greeting = 'YOU WILL GIT 10% DISCOUNT! ' ;}
-else if (Number <20 ) {
+else if (Number <=20 ) {
     greeting = 'NO DISCOUNT!!!';
     }
 
@@ -36,3 +36,11 @@ function getGreeting (Number) {
 }
 getGreeting (Number);     
 console.log ('Number of flowers required is :' + Number);
+
+
+// ask the user to prove that he/here is not a robot by adding no.between 10 and 100
+var userInput = prompt ('Please prove that your not a robot by adding number between 10 and 100');
+while (isNaN (userInput)|| userInput == ' ' || userInput<=10 || userInput > 100) {
+    userInput = prompt ('Please prove that your not a robot by adding number between 10 and 100');
+}
+console .log (userInput);
